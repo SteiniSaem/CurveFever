5c8b221e7cc2992c6387bc7d0497e3da88cc8ff3
SteiniSaem <thorste@gmail.com>
2024-01-29 16:54:30 +0000

    lagaði villu í onlyRightAngles powerup þar sem rauð (type 2) powerup höfðu áhrif á playerinn sem náði powerup-inu

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -36,7 +36,7 @@
             else{
                 for(let i in $players){
                     if(parseInt(i) != id){
-                        $players[id].onlyRightAnglesPowerupCount += 1;
+                        $players[i].onlyRightAnglesPowerupCount += 1;
                     }
                 }
             }
@@ -228,7 +228,7 @@
         let dx, dy;
         occupiedPoints = [];
         framesUntilPowerup = maxFramesUntilPowerup;
-        livePowerups = [];
+        livePowerups = [{x: 500, y: 500, power:"onlyRightAngles", type:2}];
         crashedPlayers = [];
         for(let id in $players){
             [dx, dy] = [Math.random() * (Math.round(Math.random()) ? 1 : -1), Math.random() * Math.round(Math.random()) ? 1 : -1];
